a426a491bc7e04d789af0ff2ea7f2a59e9d54241
fonsalir03 <mdav121503@gmail.com>
2026-06-22 13:47:50 -0500

    add linked list class add node class add method to add node to the end of the linked list add method to add node to the beggining of the linked list

--- LinkedList.js
+++ LinkedList.js
@@ -1,1 +1,39 @@
-console.log("hello, world!")
+class Node {
+    constructor(){
+        this.value = null
+        this.nextNode = null
+    }
+}
+
+class LinkedList {
+    #head
+    #tail
+    constructor(){
+        this.#head = null
+        this.#tail = null
+    }
+
+    append(value){
+        const newNode = new Node()
+        newNode.value = value
+
+        if (this.#head == null){
+            this.#head = newNode
+        }else if(this.#head.nextNode == null){
+            this.#tail = newNode
+            this.#head.nextNode = newNode
+        }else{
+            this.#tail.nextNode = newNode
+            this.#tail = newNode
+        }
+    }
+
+    prepend(value){
+        const newNode = new Node()
+        newNode.value = value
+
+        newNode.nextNode = this.#head
+        this.#head = newNode
+    }
+
+}
